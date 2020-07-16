13819e9e5015f488a7c287738c694f3e1b5cf6c5
shabeermsk <shabeermsk@gmail.com>
2020-07-16 14:11:47 +0530

    added error messages

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core import validators
 from django import forms
 
 
 class RegisterForm(UserCreationForm):
+    password1=forms.CharField(widget=forms.PasswordInput,validators=[validate_password])
+
     class Meta:
         model = get_user_model()
         fields = ('first_name', 'last_name', 'company', 'email', 'password1')
